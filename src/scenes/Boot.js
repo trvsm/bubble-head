d48527741aee70b5d77eb2e375c1facd5e1222c4
Travis Martin <trvsmartin@gmail.com>
2025-01-25 09:47:30 -0800

    first commit

---
+++ src/scenes/Boot.js
@@ -0,0 +1,26 @@
+import { Scene } from "phaser";
+// Scene is the base class
+// this.load is the main way to add stuff
+// create call does the stuff
+// this.physics.add adds physics props to objects, then supports things like velocity, setBounce
+// config in main.js passed to game object
+
+export class Boot extends Scene {
+  constructor() {
+    super("Boot");
+  }
+
+  preload() {
+    //  The Boot Scene is typically used to load in any assets you require for your Preloader, such as a game logo or background.
+    //  The smaller the file size of the assets, the better, as the Boot Scene itself has no preloader.
+
+    this.load.image("background", "assets/bg.png");
+    this.load.image("bubble", "assets/bubble.png");
+    this.load.image("cloud-sky", "assets/cloud-sky.png");
+    this.load.image("rock", "assets/rock.png");
+  }
+
+  create() {
+    this.scene.start("Preloader");
+  }
+}
---
+++ src/scenes/Game.js
@@ -0,0 +1,75 @@
+import { Scene } from "phaser";
+// import { EventBus } from "./EventBus";
+
+export class Game extends Scene {
+  constructor() {
+    super("Game");
+    this.player;
+    this.cursors;
+    this.bgTile;
+    this.obstacle;
+  }
+
+  create() {
+    // add tiled background for scrolling
+    this.bgTile = this.add.tileSprite(0, 0, 1024, 768, "background");
+    this.bgTile.scale = 4;
+
+    this.add
+      .text(
+        512,
+        84,
+        "Control the bubble by tilting your head left or right\nSee how far you can get without popping!",
+        {
+          fontFamily: "Arial Black",
+          fontSize: 38,
+          color: "#ffffff",
+          stroke: "#000000",
+          strokeThickness: 8,
+          align: "center",
+        }
+      )
+      .setOrigin(0.5);
+    this.add.image(512, 768, "cloud-sky");
+
+    // add bubble, that won't fall off screen
+    this.player = this.physics.add.sprite(512, 768, "bubble");
+    this.player.setCollideWorldBounds(true);
+    this.player.setBounce(0.2);
+    this.player.scale = 0.25;
+
+    // add rock to bump into
+    this.obstacle = this.physics.add.staticGroup();
+    this.obstacle.create(512, 0, "rock").refreshBody();
+    this.obstacle.scale = 0.25;
+    this.physics.add.overlap(
+      this.player,
+      this.obstacle,
+      this.hitObstacle,
+      null,
+      this
+    );
+
+    this.cursors = this.input.keyboard.createCursorKeys();
+
+    this.input.once("pointerdown", () => {
+      this.scene.start("GameOver");
+    });
+  }
+
+  update() {
+    // move the tile background
+    this.bgTile.tilePositionY -= 1;
+    // TODO: set the position of obstacle to move down
+
+    // move the bubble on keys
+    if (this.cursors.left.isDown) {
+      this.player.setVelocityX(-120);
+    } else if (this.cursors.right.isDown) {
+      this.player.setVelocityX(120);
+    }
+  }
+  hitObstacle(player, obstacle) {
+    this.scene.start("GameOver");
+  }
+}
