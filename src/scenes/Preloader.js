422cad130d1af60ea459d7292c398ed2de3ae65c
Justin Rahardjo <justindra91@gmail.com>
2025-01-25 18:20:01 -0800

    fix: go to main menu first

--- src/scenes/Preloader.js
+++ src/scenes/Preloader.js
@@ -38,14 +38,13 @@ export class Preloader extends Scene {
   preload() {
     //  Load the assets for the game - Replace with your own assets
     this.load.setPath("assets");
-
   }
 
   create() {
     //  When all the assets have loaded, it's often worth creating global objects here that the rest of the game can use.
     //  For example, you can define global animations here, so we can use them in other scenes.
     //  Move to the MainMenu. You could also swap this for a Scene Transition, such as a camera fade.
-    this.scene.start("Game");
+    this.scene.start("MainMenu");
   }
 
   getBarContainerWidth() {
